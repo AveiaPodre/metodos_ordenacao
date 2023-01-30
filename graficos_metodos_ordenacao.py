f6ecef3dac2ac0e33a8714f5258bb1678142e793
AveiaPodre <av_jr@outlook.com.br>
2023-01-30 13:41:01 -0300

    criação dos arquivos

---
+++ graficos_metodos_ordenacao.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Carregar dados
+df = pd.DataFrame({'tempo': [1, 2, 3, 4, 5],
+                   'quantidade': [20, 10, 30, 47, 100]})
+
+# Traçar gráfico
+df.plot(x='tempo', y='quantidade', kind='line')
+
+# Adicionar labels e título
+plt.xlabel('Tempo (em dias)')
+plt.ylabel('Quantidade vendida')
+plt.title('Quantidade vendida por dia')
+
+# Mostrar gráfico
+plt.show()
+
+
+#esse arquivo aqui era só um teste que eu tava fazendo
