f071fd5ee513fafd2b81416a85df3bce7d9e000e
Luis Enilton <110181744+LuisEnilton@users.noreply.github.com>
2023-01-30 20:24:20 -0300

    1
    Correção

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
+from unidecode import unidecode
 from sort_methods import *
 from time import time
 nomes = ''
 try:
-    with open(f'nomes10K.txt', 'r+', encoding='utf-8') as arquivo:
+    with open(f'nomes500K.txt', 'r+', encoding='utf-8') as arquivo:
         for nome in arquivo:
             nomes = nomes + nome
 except:
     print('Erro na leitura do arquivo!')
-
+nomes = unidecode(nomes)
 nomes = nomes.split('\n')
-print(bubble_sort(nomes))
-
+nomes = [x.lower() for x in nomes if x[0].isupper()]
+tempo_inicial = time()
+nomes = merge_sort(nomes)
+tempo_final = time()
+print(tempo_final-tempo_inicial)
